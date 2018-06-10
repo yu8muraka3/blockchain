73fdd03d8bb6b23f439083516f41108ff9d60c41
yuya <yuyatennish@gmail.com>
2018-06-10 17:30:04 +0900

    cheap algorithm for ajusting the difficulty

--- blockchain.py
+++ blockchain.py
@@ -5,15 +5,20 @@ from time import time
 from uuid import uuid4
 from textwrap import dedent
 from flask import Flask, jsonify, request
+from urllib.parse import urlparse
 
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-
+        self.difficulty = "0000"
+        self.nodes = set()
         # ジェネシスブロックを作る
         self.new_block(previous_hash=1, proof=100)
 
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
 
     def new_block(self, proof, previous_hash=None):
@@ -54,7 +59,7 @@ class Blockchain:
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
-            'amount': amount,
+            'amount': amount
         })
 
         return self.last_block['index'] + 1
@@ -92,6 +97,25 @@ class Blockchain:
             proof += 1
         return proof
 
+    def change_difficulty(self):
+        recent_blocks = blockchain.chain[-5:]
+        if len(recent_blocks) < 5:
+            return 'Missing 5 blocks', 400
+        i = 0
+        recent_generate_blocktime = 0
+        while i < 4:
+            recent_generate_blocktime += (recent_blocks[i+1]['timestamp'] - recent_blocks[i]['timestamp'])
+            i += 1
+        average_time = recent_generate_blocktime / len(recent_blocks)
+
+        if average_time > 10:
+            self.difficulty.rstrip("0")
+        elif average_time < 10:
+            self.difficulty += "0"
+
+        print(blockchain.difficulty)
+        return blockchain.difficulty
+
     @staticmethod
     def valid_proof(last_proof, proof):
         """
@@ -104,7 +128,53 @@ class Blockchain:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
 
-        return guess_hash[:1] == "0"
+        return guess_hash[:len(blockchain.difficulty)] == blockchain.difficulty
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n---------------\n")
+
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
 
 
 app = Flask(__name__)
@@ -125,7 +195,7 @@ def new_transactions():
 
         # 新しいトランザクションを作る
     index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-    response = {'message': 'トランザクションはブロック{index}に追加されました'}
+    response = {'message': f'トランザクションはブロック{index}に追加されました'}
     return jsonify(response), 201
 
 
@@ -148,11 +218,15 @@ def mine():
     # チェーンに新しいブロックを加えることで、新しいブロックを採掘する
     block = blockchain.new_block(proof)
 
+    if last_block['index'] % 5 == 0:
+        blockchain.change_difficulty()
+
     response = {
         'message': '新しいブロックを採掘しました',
         'index': block['index'],
         'transactions': block['transactions'],
         'proof': block['proof'],
+        'timestamp': block['timestamp'],
         'previous_hash': block['previous_hash'],
     }
     return jsonify(response), 200
@@ -166,6 +240,48 @@ def full_chain():
     }
     return jsonify(response), 200
 
+@app.route('/difficulty', methods=['GET'])
+def now_difficulty():
+    response = {
+        'difficulty': blockchain.difficulty
+    }
+
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_node():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: 有効ではないノードのリストです", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': '新しいノードが追加されました',
+        'total_nodes': list(blockchain.nodes),
+    }
+
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'チェーンが置き換えられました',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'チェーンが確認されました',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
 
 
 # port5000でサーバーを起動する
